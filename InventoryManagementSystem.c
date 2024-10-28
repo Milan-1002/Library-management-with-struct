83b2ad3b6cc2e6f97d2807a6103549a654c22db7
Milan-1002 <sunarmilan1002@gmail.com>
2024-10-28 01:40:59 -0500

    Added some function call

--- InventoryManagementSystem.c
+++ InventoryManagementSystem.c
@@ -26,5 +26,55 @@ struct salesRecord{
 };
 
 int main(){
+    struct inventory in[50];
+    struct salesRecord sR[50];
+
+    int totalQuantity = 0;
+    int totalSalesAmount = 0;
+    int salesRecordCount = 0;
+    printf("\nInventory Management System\n");
+    printf("1.Add a product\n");
+    printf("2.Record a sale\n");
+    printf("3.Display Inventory\n");
+    printf("4.Display sales record\n");
+    printf("5.Exit\n");
+
+    int choice;
+    printf("Enter a choice\n");
+    scanf("%d",&choice);
+
+    switch(choice){
+        do
+        {
+            case 1:
+            addProduct(in, &totalQuantity);
+            break;
+
+            case 2:
+            recordSale(sR,&totalSalesAmount,&salesRecordCount);
+            break;
+
+            case 3:
+            displayInventory(in, totalQuantity);
+            break;
+
+            case 4:
+            displaySalesRecord(sR,salesRecordCount);
+            break;
+
+            case 5:
+            printf("Exiting...\n");
+
+            default:
+            printf("\nPlease Enter a valid choice\n");
+            continue;
+
+
+            
+        } while (choice != 5);
+        
+    }
+
+
 
 }
